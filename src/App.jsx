81b477a4950fac9269bb71151eb2b0d80cf3f133
iceweasel <johnsjosephk@pm.me>
2025-04-13 23:15:36 +0530

    activity page year grid  and option selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { Routes , Route  } from 'react-router-dom'
+import Activity from './pages/Activity'
 
 function App() {
-  
-
   return (
     <>
+
     <Header/>
+      <Routes>
+          <Route path={'/activity'} element={<Activity/>} />
+      </Routes>
     <Footer/>
-      
+
     </>
   )
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 
 function Grid() {
+  const [year , setYear] = useState('');
+
+  useEffect(()=> {
+    let year = new Date().getFullYear()
+    setYear(year)
+  },[])
+
+   const handleClick = (e) => {
+      console.log(e)
+   }
+
+    const col_fill = (key,value) => {
+      // console.log(typeof(key))
+       const dayElement = [];
+       for(let i = 0 ; i < 31 ;i++){
+         if(i < value){
+            dayElement.push(
+              <button style={{}} key={`${key}-${i}`} className="border markinput hover:bg-green-500 border-dark rounded-0 border-1"  onClick={(e)=>{handleClick(e)}} variant="light"></button>
+          )
+         }else{
+          dayElement.push(
+            <button key={`${key}-${i}`} className="border markinput bg-slate-500 border-dark rounded-0 border- invisible"  onClick={(e)=>{handleClick(e)}} variant="secondary" disabled></button>
+          )
+         }
+       }
+
+       let text = key =='July'? 'Ju':key[0]
+       return <div key={`${key}`} className='d-flex flex-column'>
+                <p className="mb-0 text-dark  text-center"><b>{text}</b></p>
+                  {dayElement}
+              </div>
+
+    }
+
+    const days = [31,28,31,30,31,30,31,30,31,30,31,30,31,]
+    const months =['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const indices = new Array(31).fill(1);
+
+    const handleChange = (val) => {
+      setYear(val)
+      if ((val % 400 == 0) || (val % 4 == 0 && (val % 100 != 0))){
+        days[1] = 29;
+        console.log(days)
+      }else{
+        days[1] = 28;
+      }
+    }
+
   return (
-    <div>Grid</div>
+    <div  className="calendar-grid my-1">
+
+        <div className="flex justify-center">
+          <button className="rounded-3 bg-red-500 ms-2 px-3 py-2" onClick={()=>{handleChange(year-1)}}> - </button>
+          <span className="fs-3">Year { year }</span>
+          <button className="rounded-3 bg-green-400 px-3 py-2" onClick={()=>{handleChange(year+1)}}> + </button>
+        </div>
+
+        <div className="d-flex my-2">
+            <div className="d-flex flex-column">
+                <p className='m-0 h-[25px]'></p>
+                {
+                  indices.map( (num,idx) => <p key={`Day-${idx+1}`} className="mb-0 text-dark me-2 text-center h-[25px]">{idx+1}</p> )
+                }
+            </div>
+
+            {
+              months.map( (obj,idx) =>  col_fill(obj,days[idx])  )
+            }
+        </div>
+
+    </div>
   )
 }
 
