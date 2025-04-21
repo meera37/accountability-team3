e212ba599e1b32ba66145dc09c1fab99ed672fb4
meera37 <meerabenny370@gmail.com>
2025-04-21 17:02:25 +0530

    DeleteConfirmModal component added

--- src/components/ActivityRow.jsx
+++ src/components/ActivityRow.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DeleteConfirmModal from './DeleteConfirmModal';
 
-function ActivityRow({ activity, idx, handleOpenDescriptionModal, descriptionLength, isPublicSection, onAddTemplate, }) {
+function ActivityRow({ activity, idx, handleOpenDescriptionModal, descriptionLength, isPublicSection, onAddTemplate }) {
  
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+   
     const buttonStyle = {
         marginRight: '8px', 
       };
    
+      const handleDeleteClick = () => {
+        setIsDeleteModalOpen(true);
+    };
+
+    const handleCloseDeleteModal = () => {
+        setIsDeleteModalOpen(false);
+    };
+    
+ 
     return (
         <>
             <tr key={idx} className="border-b">
@@ -26,14 +38,14 @@ function ActivityRow({ activity, idx, handleOpenDescriptionModal, descriptionLen
                 <td className="px-4 py-2 text-center text-gray-700 text-sm">{activity.type}</td>
                 <td className="px-4 py-2 text-center">
 
-                  <div className="flex space-x-10 justify-center">
+                  <span className="flex space-x-10 justify-center">
                         <button
                         style={buttonStyle}
                             className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 text-sm">
                             View More
                         </button>
-                        <button style={buttonStyle} className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-sm">
-                            Archive
+                        <button onClick={handleDeleteClick} style={buttonStyle} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-sm">
+                            Delete
                         </button>
     
                         {activity.type === 'public' && isPublicSection && onAddTemplate && (
@@ -45,12 +57,20 @@ function ActivityRow({ activity, idx, handleOpenDescriptionModal, descriptionLen
                                 Add Template
                             </button>
                         )}
-                  </div>
+                  </span>
 
                 </td>
             </tr>
+            {isDeleteModalOpen && (
+                <DeleteConfirmModal
+                    show={isDeleteModalOpen}
+                    onClose={handleCloseDeleteModal}
+                   // onConfirmDelete={handleConfirmDelete}
+                />
+            )}
         </>
     )
 }
 
 export default ActivityRow
+
